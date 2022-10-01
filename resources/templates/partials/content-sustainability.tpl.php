1e72a9c8a9b48b8db0c0aec2f7f016f1b55f8c0c
Francesco Rho <eleven@rhofrances.co>
2022-10-01 16:19:39 +0200

    Sustainability WIP

--- resources/templates/partials/content-sustainability.tpl.php
+++ resources/templates/partials/content-sustainability.tpl.php
@@ -1,74 +1,90 @@
 <?php
-    $language = function_exists('pll_current_language') ? pll_current_language()  : ICL_LANGUAGE_CODE;
-    $sustainability_cover = $language == 'en' ? 'sustainability_cover.jpg' : '2021 Rapporto sostenibiltà Pigna_Copertina_ITA (1) (1).jpg';
-    $doc_filename = $language  == 'en' ? 'PIGNA_report21_ENG-schermo.pdf' : 'PIGNA_report21_ITA-schermo.pdf';
+$language = function_exists('pll_current_language') ? pll_current_language()  : ICL_LANGUAGE_CODE;
+$sustainability_cover_2020 = $language == 'en' ? 'sustainability_cover_2020_en.png' : 'sustainability_cover_2020_ita.png';
+$sustainability_cover_2021 = $language == 'en' ? 'sustainability_cover_2021_en.jpg' : 'sustainability_cover_2021_ita.jpg';
+$doc_filename_2020 = $language  == 'en' ? 'Sostenibilità_impaginato_eng_web.pdf' : 'Sostenibilità_impaginato_ita_web.pdf';
+$doc_filename_2021 = $language  == 'en' ? 'PIGNA_report21_ENG-schermo.pdf' : 'PIGNA_report21_ITA-schermo.pdf';
 ?>
 <div id="page-sustainability">
-    <div class="sustainability-section">
+    <div class="sustainability-section sustainability-section__1" style="position: relative; z-index: 10">
         <div class="grid-container text-center">
-            <img id="sostenibilita-icon-1"
-                 src="<?= get_stylesheet_directory_uri() ?>/public/images/sostenibilita-icon-1a.png"/>
             <div class="grid-x align-right py-180px">
-                <div class="cell small-12 medium-5 medium-text-right text-center">
+                <div class="cell small-12 medium-5 medium-text-right text-center page-background">
                     <h2 class="--text-red --font-bold"><?php _e('Sustainability', $textdomain); ?></h2>
                     <p>
-                        <?php _e('The first Pigna Sustainability Report aims to tell in a complete and transparent way what sustainability means for the company. A snapshot of the actions, results, approaches, and strategies with which Pigna wants to create lasting, widespread, and consistent value with the needs of all stakeholders and with ecological balances.', $textdomain); ?>
+                        <?php _e('The Pigna Sustainability Report aims to tell in a complete and transparent way what sustainability means for the company. A snapshot of the actions, results, approaches, and strategies with which Pigna wants to create lasting, widespread, and consistent value with the needs of all stakeholders and with ecological balances.', $textdomain); ?>
                     </p>
-                </div>
-            </div>
-            <div class="video-wrapper">
-                <video
-                        class="video-js w-full vjs-default-skin"
-                        controls
-                        poster='<?= get_stylesheet_directory_uri() ?>/public/images/sostenibilita-video-poster.png'
-                        preload="auto"
-                        data-setup='{ "aspectRatio":"1920:1080"}'
-                >
-                    <source
-                            src="<?= get_stylesheet_directory_uri() ?>/resources/assets/video/Video_Pigna-Bilancio-v3-2.mp4"
-                            type="video/mp4"/>
-                    <source
-                            src="<?= get_stylesheet_directory_uri() ?>/resources/assets/video/Video_Pigna-Bilancio-v3.webm"
-                            type="video/webm"/>
-                </video>
-                <div class="video-play-wrapper grid-x align-center">
-                    <button class="video-play"><?php _e('Watch the video', $textdomain); ?></button>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="sustainability-section">
+    <div class="sustainability-section sustainability-section__2">
         <div class="grid-container">
-            <img id="sostenibilita-icon-2"
-                 src="<?= get_stylesheet_directory_uri() ?>/public/images/sostenibilita-icon-2a.png"/>
-            <div class="grid-x align-left pt-180px">
+            <div class="grid-x align-middle">
                 <div class="cell small-12 medium-5 medium-text-left text-center">
-                    <h2 class="--text-blue --font-bold"><?php _e('The application effort', $textdomain); ?></h2>
-                    <p>
+                    <h2 class="--text-yellow --font-bold"><?php _e('The application effort', $textdomain); ?></h2>
+                    <p class="--text-white">
                         <?php _e('Pigna operates within internationally recognized guidelines such as the United Nations Global Compact, adhering to the 2030 Agenda and the Sustainable Development Goals. Today the company\'s commitment to sustainability focuses on achieving 6 of the 17 Sustainable Development Goals.', $textdomain); ?>
                     </p>
                 </div>
+                <div class="cell small-12 medium-7">
+                    <img src="<?= get_stylesheet_directory_uri() ?>/resources/assets/images/sustainability/sezione-2-1.svg">
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="sustainability-section mt-90px">
+    <div class="sustainability-section sustainability-section__3">
         <div class="grid-container">
-            <img id="sostenibilita-icon-3"
-                 src="<?= get_stylesheet_directory_uri() ?>/public/images/sostenibilita-icon-3a.png"/>
-            <div class="grid-x align-right py-90px">
-                <div class="cell small-8 medium-5 text-right sustainability-report-wrapper">
-                    <img src="<?= get_stylesheet_directory_uri() ?>/public/images/<?php echo $sustainability_cover; ?>"
-                         id="sostenibilita-report-cover"/>
+            <div class="grid-x">
+                <div class="cell medium-3">
+
+                </div>
+                <div class="cell small-8 medium-6 sustainability-report-wrapper">
+                    <img src="<?= get_stylesheet_directory_uri() ?>/public/images/sustainability/<?php echo $sustainability_cover_2021; ?>" id="sostenibilita-report-cover" />
                     <div class="grid-x align-center">
-                        <a class="btn-report"
-                           href="<?= get_stylesheet_directory_uri() ?>/resources/assets/pdf/<?php echo $doc_filename; ?>"><?php _e('Download report', $textdomain); ?>
-                        </a>
+                        <a target="_blank" class="btn-report" href="<?= get_stylesheet_directory_uri() ?>/resources/assets/pdf/<?php echo $doc_filename_2021; ?>"><?php _e('Download report', $textdomain); ?></a>
+                    </div>
+                </div>
+                <div class="cell medium-3">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sustainability-section sustainability-section__4">
+        <div class="grey-bk">
+            <div class="grid-container">
+                <div class="grid-x align-middle">
+                    <div class="cell small-12">
+                        <h2 class="--text-red --font-bold"><?php _e('Balance and Report', $textdomain); ?></h2>
+                    </div>
+                    <div class="cell small-2 medium-4" style="position: relative;">
+                        <img src="<?= get_stylesheet_directory_uri() ?>/public/images/sustainability/<?php echo $sustainability_cover_2021; ?>" id="sostenibilita-report-cover" />
+                        <div class="vt-line"></div>
+                    </div>
+                    <div class="cell small-10 medium-8">
+                        <h3><?php _e('Balance 2021', $textdomain); ?></h3>
+                        <a target="_blank" class="btn-report" href="<?= get_stylesheet_directory_uri() ?>/resources/assets/pdf/<?php echo $doc_filename_2021; ?>"><?php _e('Download report 2021', $textdomain); ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="white-bk">
+            <div class="grid-container">
+                <div class="grid-x align-middle">
+                    <div class="cell small-2 medium-4" style="position: relative;">
+                        <img src="<?= get_stylesheet_directory_uri() ?>/public/images/sustainability/<?php echo $sustainability_cover_2020; ?>" id="sostenibilita-report-cover" />
+                        <div class="vt-line"></div>
+                    </div>
+                    <div class="cell small-10 medium-8">
+                        <h3><?php _e('Balance 2020', $textdomain); ?></h3>
+                        <a target="_blank" class="btn-report green" href="<?= get_stylesheet_directory_uri() ?>/resources/assets/pdf/<?php echo $doc_filename_2020; ?>"><?php _e('Download report 2020', $textdomain); ?></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script src="https://vjs.zencdn.net/7.15.4/video.min.js"></script>
---
+++ resources/templates/partials/zaini.php
@@ -0,0 +1,28 @@
+<div id="zaini" class="orbit zaini" role="region"
+     data-timer-delay="5000"
+     aria-label="Diari Slider" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+    <div class="orbit-wrapper">
+        <ul class="orbit-container">
+            <li class="is-active orbit-slide">
+                <figure class="orbit-figure grid-x align-middle slider-1">
+
+                    <div class="grid-container full" style="width: 100%">
+                        <div class="grid-x grid-x align-middle">
+                            <div class='cell large-1 medium-1'></div>
+                            <div class="cell small-12 medium-12 large-5">
+                                <h1><?php _e('friend of colors', $textdomain); ?></h1>
+                                <p>
+                                    <?php _e('From school to free time, what matters is to give a unique nuance to your daily adventures in the company of Monochrome backpacks', $textdomain); ?>
+                                </p>
+
+                            </div>
+                            <div class="cell hide-for-small-only medium-6 large-5">
+
+                            </div>
+                        </div>
+                    </div>
+                </figure>
+            </li>
+        </ul>
+    </div>
+</div>
