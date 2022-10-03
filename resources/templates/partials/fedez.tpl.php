65a5a43817e51d3eca226501b3c347dd28eb9032
Francesco Rho <eleven@rhofrances.co>
2022-10-03 21:19:13 +0200

    Alberto fix

--- resources/templates/partials/fedez.tpl.php
+++ resources/templates/partials/fedez.tpl.php
@@ -10,5 +10,23 @@
             <img class="hide-for-medium" src="<?= get_stylesheet_directory_uri() ?>/resources/assets/images/Monocromo_Pop_Up_Jeremy_Ville_1280x1920_EN.jpg">
         </a>
     <?php } ?>
+    <p onclick="hideFedezModal()" style="position: absolute;top: 15px;right: 15px;font-weight: 600;text-transform: uppercase;cursor: pointer;">chiudi</p>
 </div>
 
+<script type="text/javascript">
+    function hideFedezModal() {
+        var fdOverlay = document.getElementsByClassName('reveal-overlay')[0]
+        var htmlEl = document.getElementsByTagName('html')[0]
+
+        if(fdOverlay && htmlEl) {
+            fdOverlay.remove()
+            htmlEl.classList.remove('is-reveal-open')
+        }
+    }
+</script>
+<style>
+    #fedezModal {
+        padding-top: 50px !important;
+        background-color: #cbe9f7 !important;
+    }
+</style>
